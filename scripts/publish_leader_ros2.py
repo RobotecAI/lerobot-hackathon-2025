9c50bff7290b0e8bd7595c4bfa329f7b3c0a625d
Bartłomiej Boczek <bartlomiej.boczek@robotec.ai>
2025-06-15 11:46:36 +0200

    update script for ros2 publishing from leader

--- scripts/publish_leader_ros2.py
+++ scripts/publish_leader_ros2.py
@@ -25,24 +25,54 @@ teleop_device = SO101Leader(teleop_config)
 teleop_device.connect()
 
 rclpy.init()
-node = rclpy.create_node("leader_pub")
-publisher = node.create_publisher(Joints, "joints", 10)
 
-while True:
-    action = teleop_device.get_action()
-    joints = Joints(
-        shoulder_pan=action['shoulder_pan.pos'],
-        shoulder_lift=action['shoulder_lift.pos'],
-        elbow_flex=action['elbow_flex.pos'],
-        wrist_flex=action['wrist_flex.pos'],
-        wrist_roll=action['wrist_roll.pos'],
-        gripper=action['gripper.pos'],
-    )
-    print(joints.model_dump_json())
+class MyNode(rclpy.node.Node):
+    def __init__(self):
+        super().__init__("leader_pub")
 
-    position_command = Float64MultiArray()
-    position_command.data = [joints.shoulder_pan, joints.shoulder_lift, joints.elbow_flex, joints.wrist_flex, joints.wrist_roll, joints.gripper]
-    publisher.publish(position_command)
+        self.publisher = self.create_publisher(Float64MultiArray, '/base/position_controller/commands', 10)
+        
+        self.timer = self.create_timer(1/30.0, self.timer_callback)
+        self.maxes = [0,0,0,0,0,0]
+        self.mins = [0,0,0,0,0,0]
+
+    def timer_callback(self):
+        action = teleop_device.get_action()
+        joints = Joints(
+            shoulder_pan=action['shoulder_pan.pos'] * 0.009599315,
+            shoulder_lift=action['shoulder_lift.pos'] * 0.0279253,
+            elbow_flex=action['elbow_flex.pos'] * 0.0165806,
+            wrist_flex=action['wrist_flex.pos'] * 0.01658065,
+            wrist_roll=action['wrist_roll.pos'] * 0.0174533,
+            gripper=action['gripper.pos'] * 0.01919863,
+        )
+        print(joints.model_dump_json())
+        
+
+        position_command = Float64MultiArray()
+        position_command.data = [joints.shoulder_pan, joints.shoulder_lift, joints.elbow_flex, joints.wrist_flex, joints.wrist_roll, joints.gripper]
+        self.publisher.publish(position_command)
+
+
+node = MyNode()
+rclpy.spin(node)
+# publisher = node.create_publisher(Float64MultiArray, '/position_controller/commands', 10)
+#
+# while True:
+#     action = teleop_device.get_action()
+#     joints = Joints(
+#         shoulder_pan=action['shoulder_pan.pos'],
+#         shoulder_lift=action['shoulder_lift.pos'],
+#         elbow_flex=action['elbow_flex.pos'],
+#         wrist_flex=action['wrist_flex.pos'],
+#         wrist_roll=action['wrist_roll.pos'],
+#         gripper=action['gripper.pos'],
+#     )
+#     print(joints.model_dump_json())
+#
+#     position_command = Float64MultiArray()
+#     position_command.data = [joints.shoulder_pan, joints.shoulder_lift, joints.elbow_flex, joints.wrist_flex, joints.wrist_roll, joints.gripper]
+#     publisher.publish(position_command)
 
 # uv run python -m lerobot.teleoperate --robot.type=so101_follower --robot.port=/dev/ttyACM0 --robot.id=follower --teleop.type=so101_leader --teleop.port=/dev/ttyACM1 --teleop.id=leader
 # uv run python -m lerobot.calibrate --teleop.type=so101_leader --teleop.port=/dev/ttyACM1 --teleop.id=leader
